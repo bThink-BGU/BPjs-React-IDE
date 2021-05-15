e492b13461fec4555aa745f4b94cd91d12f64afe
Avishai Yaniv <avishai@innovid.com>
2021-05-15 17:39:17 +0300

    added scroller to left panel

--- src/pages/IDE/ide.styles.jsx
+++ src/pages/IDE/ide.styles.jsx
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
+const size = {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopL: '1440px',
+    desktop: '2560px'
+}
+
+export const device = {
+    mobileS: `(max-height: ${size.mobileS})`,
+    mobileM: `(max-height: ${size.mobileM})`,
+    mobileL: `(max-height: ${size.mobileL})`,
+    tablet: `(max-height: ${size.tablet})`,
+    laptop: `(max-height: ${size.laptop})`,
+    laptopL: `(max-height: ${size.laptopL})`,
+    desktop: `(max-height: ${size.desktop})`,
+    desktopL: `(max-height: ${size.desktop})`
+};
+
 export const IdeContentContainer = styled.div`
   display: flex;
   justify-content: flex-start;
